84576b5b97ee604501ddd6e4f7b4cbb5aa678d8f
Jordan Kniest <j.kniest@basecom.de>
2024-04-11 11:03:49 +0200

    Remove Fixture Bag (#53)
    * Drop support for sw < 6.5, drop support for php8.1, add support for shopware 6.6 & php 8.3
    
    * Update command structure, add readonly to attributes and classes
    
    * Update prettier
    
    * Update min phpstan version
    
    * Remove fixturebag
    
    * Remove noinspection attribute

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -12,6 +12,7 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 ### Removed
 - Dropped support for PHP 8.1
 - Dropped support for Shopware 6.3 & 6.4
+- Removed FixtureBag
 
 ## [2.4.0] - 2023-11-15
 ### Added
--- src/Fixture.php
+++ src/Fixture.php
@@ -6,7 +6,7 @@ namespace Basecom\FixturePlugin;
 
 abstract readonly class Fixture
 {
-    abstract public function load(FixtureBag $bag): void;
+    abstract public function load(): void;
 
     /** @return string[] */
     public function dependsOn(): array
